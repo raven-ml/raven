1f05d410d5896f4ba3405d5928e43d7ac448a521
Thibaut Mattio <thibaut.mattio@gmail.com>
2025-04-21 12:43:55 -0400

    Move focused state to the inline and block in the model

--- quill/src/editor/model.ml
+++ quill/src/editor/model.ml
@@ -1,57 +1,75 @@
-type inline =
+type inline_content =
   | Run of string
-  | Emph of inline
-  | Strong of inline
+  | Emph of inline_content
+  | Strong of inline_content
   | Seq of inline list
+
+and inline = { id : int; content : inline_content; focused : bool }
 
 type block_content =
   | Paragraph of inline
   | Codeblock of string
   | Blocks of block list
 
-and block = { id : int; content : block_content }
+and block = { id : int; content : block_content; focused : bool }
 
-type model = { document : block list; focused : (int * int) option }
+type model = { document : block list }
 
 let next_id = ref 0
 
 let init : model =
-  let mk id content = { id; content } in
+  let mk_block id content : block = { id; content; focused = false } in
+  let mk_inline id content : inline = { id; content; focused = false } in
   {
     document =
       [
-        mk 0 (Paragraph (Run "Welcome to Quill!"));
-        mk 1
+        mk_block 0 (Paragraph (mk_inline 0 (Run "Welcome to Quill!")));
+        mk_block 1
           (Paragraph
-             (Seq [ Run "This is a "; Emph (Run "rich"); Run " text editor." ]));
+             (mk_inline 1
+                (Seq
+                   [
+                     mk_inline 2 (Run "This is a ");
+                     mk_inline 3 (Emph (Run "rich"));
+                     mk_inline 4 (Run " text editor.");
+                   ])));
       ];
-    focused = None;
   }
 
 let rec inline_of_cmarkit inline =
   let open Cmarkit in
+  let mk content : inline option =
+    let id = !next_id in
+    incr next_id;
+    Some { id; content; focused = false }
+  in
   match inline with
-  | Inline.Text (s, _) -> Some (Run s)
+  | Inline.Text (s, _) -> mk (Run s)
   | Inline.Emphasis (inner, _) -> (
       match inline_of_cmarkit (Inline.Emphasis.inline inner) with
-      | Some i -> Some (Emph i)
-      | None -> Some (Emph (Run "")))
+      | Some i -> mk (Emph i.content)
+      | None -> mk (Emph (Run "")))
   | Inline.Strong_emphasis (inner, _) -> (
       match inline_of_cmarkit (Inline.Emphasis.inline inner) with
-      | Some i -> Some (Strong i)
-      | None -> Some (Strong (Run "")))
+      | Some i -> mk (Strong i.content)
+      | None -> mk (Strong (Run "")))
   | Inline.Inlines (items, _) ->
       let inlines = List.filter_map inline_of_cmarkit items in
-      if inlines = [] then None else Some (Seq inlines)
+      if inlines = [] then None else mk (Seq inlines)
   | _ -> None
 
-let rec block_content_of_cmarkit (cb : Cmarkit.Block.t) : block_content =
+let rec block_content_of_cmarkit cb =
   let open Cmarkit in
   match cb with
   | Block.Paragraph (p, _) ->
       let norm = Inline.normalize (Block.Paragraph.inline p) in
       let inline =
-        match inline_of_cmarkit norm with Some i -> i | None -> Run ""
+        match inline_of_cmarkit norm with
+        | Some i -> i
+        | None ->
+            let id = !next_id in
+            incr next_id;
+            { id; content = Run ""; focused = false }
       in
       Paragraph inline
   | Block.Code_block (codeblock, _) ->
@@ -63,21 +81,17 @@ let rec block_content_of_cmarkit (cb : Cmarkit.Block.t) : block_content =
       in
       Codeblock code
   | Block.Blocks (items, _) ->
-      (* recursively convert each sub‑block to a `block` below *)
       let children = List.map block_of_cmarkit items in
       Blocks children
-  | _ ->
-      (* fallback for any other Cmarkit blocks *)
-      Paragraph (Run "")
+  | _ -> Paragraph { id = !next_id; content = Run ""; focused = false }
 
-and block_of_cmarkit (cb : Cmarkit.Block.t) : block =
+and block_of_cmarkit cb : block =
   let id = !next_id in
   incr next_id;
-  { id; content = block_content_of_cmarkit cb }
+  { id; content = block_content_of_cmarkit cb; focused = false }
 
-and document_of_cmarkit (root : Cmarkit.Block.t) : block list =
+and document_of_cmarkit root =
   next_id := 0;
-  (* start IDs at 0 *)
   match Cmarkit.Block.normalize root with
   | Cmarkit.Block.Blocks (items, _) -> List.map block_of_cmarkit items
   | other -> [ block_of_cmarkit other ]
--- quill/src/editor/view.ml
+++ quill/src/editor/view.ml
@@ -5,28 +5,25 @@ open Vdom
 let span ?key ?a l = elt "span" ?key ?a l
 let p ?key ?a l = elt "p" ?key ?a l
 let tabindex = int_attr "tabindex"
-let get_segments = function Seq rs -> rs | r -> [ r ]
 
-let rec inline_to_plain = function
+let get_segments (inline : inline) =
+  match inline.content with Seq rs -> rs | _ -> [ inline ]
+
+let rec inline_content_to_plain (inline_content : inline_content) =
+  match inline_content with
   | Run s -> s
-  | Emph i -> inline_to_plain i
-  | Strong i -> inline_to_plain i
+  | Emph i -> inline_content_to_plain i
+  | Strong i -> inline_content_to_plain i
   | Seq items -> String.concat "" (List.map inline_to_plain items)
 
-let wrap_run ?focused ~block ~run_j run =
+and inline_to_plain (inline : inline) = inline_content_to_plain inline.content
+
+let wrap_run ~block ~run_j (run : inline) =
   let id = Printf.sprintf "block-%d-run-%d" block run_j in
-  let txt =
-    match run with
-    | Run s -> s
-    | Emph _ | Strong _ | Seq _ -> inline_to_plain run
-  in
-  let focused =
-    match focused with
-    | Some (b, r) when b = block && r = run_j -> true
-    | _ -> false
-  in
+  let txt = inline_to_plain run in
+  let focused = run.focused in
   let attrs = [ attr "id" id; tabindex 0 ] in
-  match run with
+  match run.content with
   | Run s -> span ~key:id ~a:attrs [ text s ]
   | Emph _ ->
       span ~key:id ~a:attrs
@@ -36,27 +33,23 @@ let wrap_run ?focused ~block ~run_j run =
           span ~a:[ bool_prop "hidden" (not focused) ] [ text "*" ];
         ]
   | Strong _ ->
-      (* wrap in <strong> *)
       span ~key:id ~a:attrs
         [
           span ~a:[ bool_prop "hidden" (not focused) ] [ text "**" ];
           elt "strong" [ text txt ];
           span ~a:[ bool_prop "hidden" (not focused) ] [ text "**" ];
         ]
-  | Seq _ ->
-      (* Seq should have been flattened by get_segments *)
-      failwith "unexpected Seq in wrap_run"
+  | Seq _ -> failwith "unexpected Seq in wrap_run"
 
-let paragraph ?focused block_id inline =
+let paragraph block_id inline =
   let segs = get_segments inline in
   let children =
-    List.mapi (fun j run -> wrap_run ?focused ~block:block_id ~run_j:j run) segs
+    List.mapi (fun j run -> wrap_run ~block:block_id ~run_j:j run) segs
   in
   let id = Printf.sprintf "block-%d" block_id in
   p ~key:id ~a:[ attr "id" id ] children
 
 let codeblock block_id code =
-  (* with pre code *)
   let id = Printf.sprintf "block-%d" block_id in
   elt "pre" ~key:id
     [
