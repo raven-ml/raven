e452a3267da1344d3c885f31387471b4620dc55b
Thibaut Mattio <thibaut.mattio@gmail.com>
2025-04-21 12:43:22 -0400

    Delegate cursor management and events to submodules

---
+++ quill/src/editor/dom_utils.ml
@@ -0,0 +1,73 @@
+open Js_of_ocaml
+
+let get_current_range () =
+  let selection = Dom_html.window##getSelection in
+  if selection##.rangeCount > 0 then Some (selection##getRangeAt 0) else None
+
+let starts_with str prefix =
+  let len = String.length prefix in
+  if String.length str < len then false else String.sub str 0 len = prefix
+
+let map_node_to_indices (node : Dom.node Js.t) : (int * int) option =
+  let rec find_span (node : Dom.node Js.t) =
+    if node##.nodeType = Dom.ELEMENT then
+      let el : Dom_html.element Js.t = Js.Unsafe.coerce node in
+      let id = Js.to_string el##.id in
+      if starts_with id "block-" then Some el
+      else
+        match Js.Opt.to_option el##.parentNode with
+        | Some parent -> find_span parent
+        | None -> None
+    else
+      match Js.Opt.to_option node##.parentNode with
+      | Some parent -> find_span parent
+      | None -> None
+  in
+  match find_span node with
+  | Some span_el ->
+      let id = Js.to_string span_el##.id in
+      let parts = String.split_on_char '-' id in
+      if List.length parts >= 4 then (* Expecting "block-i-run-j" *)
+        let block_idx = int_of_string (List.nth parts 1) in
+        let inline_idx = int_of_string (List.nth parts 3) in
+        Some (block_idx, inline_idx)
+      else None
+  | None -> None
+
+type cursor_pos = { block : int; run : int; offs : int }
+
+let save_cursor () =
+  let sel = Dom_html.window##getSelection in
+  if Js.to_bool sel##.isCollapsed then
+    match get_current_range () with
+    | None -> None
+    | Some range -> (
+        let container = range##.startContainer in
+        let offset = range##.startOffset in
+        match map_node_to_indices container with
+        | None -> None
+        | Some (blk, run) -> Some { block = blk; run; offs = offset })
+  else None
+
+let restore_cursor (pos : cursor_pos option) : unit =
+  match pos with
+  | None -> ()
+  | Some { block; run; offs } -> (
+      let id = Printf.sprintf "block-%d-run-%d" block run in
+      match Dom_html.getElementById_opt id with
+      | None -> ()
+      | Some span_el ->
+          Js.Opt.iter span_el##.firstChild (fun node ->
+              let len =
+                Js.Opt.get node##.nodeValue (fun () -> Js.string "")
+                |> Js.to_string |> String.length
+              in
+              let o = if offs > len then len else offs in
+
+              let range = Dom_html.document##createRange in
+              ignore (range##setStart node o);
+              ignore (range##collapse Js._true);
+              Js_of_ocaml.Console.console##log range;
+              let sel = Dom_html.window##getSelection in
+              sel##removeAllRanges;
+              sel##addRange range))
