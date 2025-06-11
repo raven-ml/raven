a1dad9390af782b9b96c06922a191cff81fc5712
Thibaut Mattio <thibaut.mattio@gmail.com>
2025-06-11 12:18:16 -0400

    refactor(nx): Use Metal kernels for assign and gather operations
    Replace CPU fallback implementations with proper Metal GPU kernels:
    - assign now uses the assign_strided_* kernels for strided-to-contiguous copies
    - gather now uses the gather_* kernels for NumPy-style gather operations
    - CPU fallback is retained only for less common cases (strided destinations)

--- nx/lib/metal/ops_special.ml
+++ nx/lib/metal/ops_special.ml
@@ -195,24 +195,132 @@ let cast : type a b c d.
         out
 
 let assign ctx dst src =
-  (* For now, just copy the data *)
-  Ops_movement.copy ctx src dst
+  (* Check if destination is contiguous - if not, fall back to CPU *)
+  if
+    not
+      (View.is_c_contiguous dst.Internal.view
+      && View.offset dst.Internal.view = 0)
+  then
+    (* Destination is strided - use CPU fallback for now *)
+    Internal.with_command_buffer ctx (fun _cmd_buffer ->
+        (* Get CPU-accessible arrays *)
+        let get_data : type a b.
+            (a, b) Internal.t -> (a, b, Bigarray.c_layout) Bigarray.Array1.t =
+         fun t ->
+          let contents = Metal.Buffer.contents t.buffer.buffer in
+          let kind = Dtype.to_bigarray_kind t.dtype in
+          let elem_size = Internal.sizeof_dtype t.dtype in
+          let buffer_size = t.buffer.size_bytes / elem_size in
+          Ctypes.bigarray_of_ptr Ctypes.array1 buffer_size kind
+            (Obj.magic contents)
+        in
+
+        let src_arr = get_data src in
+        let dst_arr = get_data dst in
+
+        let shape = Internal.shape src in
+        let size = Internal.numel src in
+
+        (* Copy element by element, respecting views *)
+        let md_idx = Array.make (Array.length shape) 0 in
+        for linear_idx = 0 to size - 1 do
+          (* Get multi-dimensional index *)
+          Shape.unravel_index_into linear_idx shape md_idx;
+
+          (* Get offsets in source and destination *)
+          let src_offset = View.linear_index src.view md_idx in
+          let dst_offset = View.linear_index dst.view md_idx in
+
+          (* Copy value *)
+          let value = Bigarray.Array1.unsafe_get src_arr src_offset in
+          Bigarray.Array1.unsafe_set dst_arr dst_offset value
+        done)
+  else
+    (* Destination is contiguous - use Metal kernel *)
+    let shape = Internal.shape src in
+    let ndim = Array.length shape in
+    let out_size = Internal.numel src in
+
+    let dtype_suffix = Internal.dtype_to_metal_type src.dtype in
+    let kernel_name = Printf.sprintf "assign_strided_%s" dtype_suffix in
+    let func = Kernels.get_special_kernel ctx kernel_name in
+    let pipeline = Kernels.create_compute_pipeline ctx.device func in
+
+    Internal.with_command_buffer ctx (fun cmd_buffer ->
+        let encoder = ComputeCommandEncoder.on_buffer cmd_buffer in
+
+        ComputeCommandEncoder.set_compute_pipeline_state encoder pipeline;
+        ComputeCommandEncoder.set_buffer encoder ~offset:0 ~index:0
+          dst.Internal.buffer.buffer;
+        ComputeCommandEncoder.set_buffer encoder ~offset:0 ~index:1
+          src.Internal.buffer.buffer;
+
+        (* Set shape array *)
+        let shape_arr = Ctypes.(allocate_n uint32_t ~count:ndim) in
+        for i = 0 to ndim - 1 do
+          Ctypes.(shape_arr +@ i <-@ Unsigned.UInt32.of_int shape.(i))
+        done;
+        ComputeCommandEncoder.set_bytes encoder
+          ~bytes:Ctypes.(to_voidp shape_arr)
+          ~length:(ndim * 4) ~index:2;
+
+        (* Set source strides *)
+        let src_strides = View.strides src.Internal.view in
+        let strides_arr = Ctypes.(allocate_n int32_t ~count:ndim) in
+        for i = 0 to ndim - 1 do
+          Ctypes.(strides_arr +@ i <-@ Int32.of_int src_strides.(i))
+        done;
+        ComputeCommandEncoder.set_bytes encoder
+          ~bytes:Ctypes.(to_voidp strides_arr)
+          ~length:(ndim * 4) ~index:3;
+
+        (* Set ndim *)
+        let ndim_val =
+          Ctypes.(allocate uint32_t (Unsigned.UInt32.of_int ndim))
+        in
+        ComputeCommandEncoder.set_bytes encoder
+          ~bytes:Ctypes.(to_voidp ndim_val)
+          ~length:4 ~index:4;
+
+        (* Set source offset *)
+        let src_offset =
+          Ctypes.(
+            allocate uint32_t
+              (Unsigned.UInt32.of_int (View.offset src.Internal.view)))
+        in
+        ComputeCommandEncoder.set_bytes encoder
+          ~bytes:Ctypes.(to_voidp src_offset)
+          ~length:4 ~index:5;
+
+        let threads_per_group, num_groups =
+          Internal.compute_thread_groups out_size
+        in
+        let grid_size =
+          { Metal.Size.width = num_groups; height = 1; depth = 1 }
+        in
+        let group_size =
+          { Metal.Size.width = threads_per_group; height = 1; depth = 1 }
+        in
+
+        ComputeCommandEncoder.dispatch_threadgroups encoder
+          ~threadgroups_per_grid:grid_size ~threads_per_threadgroup:group_size;
+        ComputeCommandEncoder.end_encoding encoder)
 
 let gather ctx data indices axis =
   let data_shape = Internal.shape data in
   let indices_shape = Internal.shape indices in
   let ndim = Array.length data_shape in
+  let indices_ndim = Array.length indices_shape in
   let axis = if axis < 0 then ndim + axis else axis in
 
-  (* Output shape: data.shape[:axis] + indices.shape + data.shape[axis+1:] *)
-  let out_shape =
-    Array.concat
-      [
-        Array.sub data_shape 0 axis;
-        indices_shape;
-        Array.sub data_shape (axis + 1) (ndim - axis - 1);
-      ]
-  in
+  (* Validate inputs - must have same rank *)
+  if ndim <> indices_ndim then
+    invalid_arg
+      (Printf.sprintf "gather: data rank (%d) and indices rank (%d) must match"
+         ndim indices_ndim);
+
+  (* For NumPy-style gather, output shape is same as indices shape *)
+  let out_shape = indices_shape in
   let out_size = Array.fold_left ( * ) 1 out_shape in
   let size_bytes = out_size * Internal.sizeof_dtype data.Internal.dtype in
   let buffer = Buffer_pool.allocate ctx.Internal.pool size_bytes in
@@ -225,20 +333,8 @@ let gather ctx data indices axis =
       view = View.create out_shape;
     }
   in
-  let out = { out with view = View.create out_shape } in
 
-  (* Compute dimensions for gather *)
-  let axis_size = data_shape.(axis) in
-  let inner_size =
-    let rec prod i = if i >= ndim then 1 else data_shape.(i) * prod (i + 1) in
-    prod (axis + 1)
-  in
-  let outer_size =
-    let rec prod i = if i >= axis then 1 else data_shape.(i) * prod (i + 1) in
-    prod 0
-  in
-  let indices_size = Internal.numel indices in
-
+  (* Use Metal kernel for NumPy-style gather *)
   let dtype_suffix = Internal.dtype_to_metal_type data.dtype in
   let kernel_name = Printf.sprintf "gather_%s" dtype_suffix in
   let func = Kernels.get_special_kernel ctx kernel_name in
@@ -255,35 +351,77 @@ let gather ctx data indices axis =
       ComputeCommandEncoder.set_buffer encoder ~offset:0 ~index:2
         indices.Internal.buffer.buffer;
 
-      let axis_size_val =
-        Ctypes.(allocate uint32_t (Unsigned.UInt32.of_int axis_size))
+      (* Set output shape *)
+      let shape_arr = Ctypes.(allocate_n uint32_t ~count:ndim) in
+      for i = 0 to ndim - 1 do
+        Ctypes.(shape_arr +@ i <-@ Unsigned.UInt32.of_int out_shape.(i))
+      done;
+      ComputeCommandEncoder.set_bytes encoder
+        ~bytes:Ctypes.(to_voidp shape_arr)
+        ~length:(ndim * 4) ~index:3;
+
+      (* Set data shape *)
+      let data_shape_arr = Ctypes.(allocate_n uint32_t ~count:ndim) in
+      for i = 0 to ndim - 1 do
+        Ctypes.(data_shape_arr +@ i <-@ Unsigned.UInt32.of_int data_shape.(i))
+      done;
+      ComputeCommandEncoder.set_bytes encoder
+        ~bytes:Ctypes.(to_voidp data_shape_arr)
+        ~length:(ndim * 4) ~index:4;
+
+      (* Set data strides *)
+      let data_strides = View.strides data.Internal.view in
+      let strides_arr = Ctypes.(allocate_n int32_t ~count:ndim) in
+      for i = 0 to ndim - 1 do
+        Ctypes.(strides_arr +@ i <-@ Int32.of_int data_strides.(i))
+      done;
+      ComputeCommandEncoder.set_bytes encoder
+        ~bytes:Ctypes.(to_voidp strides_arr)
+        ~length:(ndim * 4) ~index:5;
+
+      (* Set indices strides *)
+      let indices_strides = View.strides indices.Internal.view in
+      let indices_strides_arr = Ctypes.(allocate_n int32_t ~count:ndim) in
+      for i = 0 to ndim - 1 do
+        Ctypes.(indices_strides_arr +@ i <-@ Int32.of_int indices_strides.(i))
+      done;
+      ComputeCommandEncoder.set_bytes encoder
+        ~bytes:Ctypes.(to_voidp indices_strides_arr)
+        ~length:(ndim * 4) ~index:6;
+
+      (* Set ndim *)
+      let ndim_val = Ctypes.(allocate uint32_t (Unsigned.UInt32.of_int ndim)) in
+      ComputeCommandEncoder.set_bytes encoder
+        ~bytes:Ctypes.(to_voidp ndim_val)
+        ~length:4 ~index:7;
+
+      (* Set axis *)
+      let axis_val = Ctypes.(allocate uint32_t (Unsigned.UInt32.of_int axis)) in
+      ComputeCommandEncoder.set_bytes encoder
+        ~bytes:Ctypes.(to_voidp axis_val)
+        ~length:4 ~index:8;
+
+      (* Set offsets *)
+      let data_offset =
+        Ctypes.(
+          allocate uint32_t
+            (Unsigned.UInt32.of_int (View.offset data.Internal.view)))
       in
-      let inner_size_val =
-        Ctypes.(allocate uint32_t (Unsigned.UInt32.of_int inner_size))
-      in
-      let indices_size_val =
-        Ctypes.(allocate uint32_t (Unsigned.UInt32.of_int indices_size))
-      in
-      let outer_size_val =
-        Ctypes.(allocate uint32_t (Unsigned.UInt32.of_int outer_size))
+      let indices_offset =
+        Ctypes.(
+          allocate uint32_t
+            (Unsigned.UInt32.of_int (View.offset indices.Internal.view)))
       in
 
       ComputeCommandEncoder.set_bytes encoder
-        ~bytes:Ctypes.(to_voidp axis_size_val)
-        ~length:4 ~index:3;
+        ~bytes:Ctypes.(to_voidp data_offset)
+        ~length:4 ~index:9;
       ComputeCommandEncoder.set_bytes encoder
-        ~bytes:Ctypes.(to_voidp inner_size_val)
-        ~length:4 ~index:4;
-      ComputeCommandEncoder.set_bytes encoder
-        ~bytes:Ctypes.(to_voidp indices_size_val)
-        ~length:4 ~index:5;
-      ComputeCommandEncoder.set_bytes encoder
-        ~bytes:Ctypes.(to_voidp outer_size_val)
-        ~length:4 ~index:6;
+        ~bytes:Ctypes.(to_voidp indices_offset)
+        ~length:4 ~index:10;
 
-      let total_work = outer_size * indices_size * inner_size in
       let threads_per_group, num_groups =
-        Internal.compute_thread_groups total_work
+        Internal.compute_thread_groups out_size
       in
       let grid_size =
         { Metal.Size.width = num_groups; height = 1; depth = 1 }
