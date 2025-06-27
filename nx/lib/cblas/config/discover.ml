03bb712d3c1d2019ba8b0a745661ba95d0b24fd6
Thibaut Mattio <thibaut.mattio@gmail.com>
2025-06-27 11:17:40 -0500

    chore(nx): Update discovery script to always use base flags

--- nx/lib/cblas/config/discover.ml
+++ nx/lib/cblas/config/discover.ml
@@ -4,7 +4,7 @@ module C = Configurator.V1
 let () =
   C.main ~name:"nx_cblas" (fun c ->
       (* Discover BLAS library *)
-      let blas_libs =
+      let blas_libs_base =
         match C.ocaml_config_var c "system" with
         | Some "macosx" ->
             (* On macOS, use Accelerate framework *)
@@ -29,33 +29,45 @@ let () =
       in
 
       (* Discover OpenMP support and platform-specific flags *)
-      let base_flags =
-        match C.ocaml_config_var c "system" with
-        | Some "macosx" ->
-            (* Use new BLAS interface on macOS to avoid deprecation
-               warnings *)
-            [ "-DACCELERATE_NEW_LAPACK"; "-O3"; "-march=native" ]
-        | _ -> [ "-O3"; "-march=native" ]
-      in
-      let test_openmp c_flags link_flags =
-        C.c_test c ~c_flags:(c_flags @ base_flags) ~link_flags
-          "#include <omp.h>\nint main() { return omp_get_num_threads(); }"
-      in
-      let omp_c_flags, omp_libs =
-        if test_openmp ["-fopenmp"] ["-lgomp"] then
-          (["-fopenmp"], ["-lgomp"])
-        else if test_openmp ["-fopenmp"] [] then
-          (["-fopenmp"], [])
-        else if test_openmp ["-Xpreprocessor"; "-fopenmp"] ["-lomp"] then
+      let c_flags, has_openmp =
+        let base_flags =
+          match C.ocaml_config_var c "system" with
+          | Some "macosx" ->
+              (* Use new BLAS interface on macOS to avoid deprecation
+                 warnings *)
+              [ "-DACCELERATE_NEW_LAPACK"; "-O3"; "-march=native" ]
+          | _ -> [ "-O3"; "-march=native" ]
+        in
+        let test_openmp flags =
+          C.c_test c ~c_flags:flags
+            "#include <omp.h>\nint main() { return omp_get_num_threads(); }"
+        in
+        if test_openmp [ "-fopenmp" ] then ("-fopenmp" :: base_flags, true)
+        else if test_openmp [ "-Xpreprocessor"; "-fopenmp" ] then
           (* macOS with Homebrew libomp *)
-          (["-Xpreprocessor"; "-fopenmp"], ["-lomp"])
+          ("-Xpreprocessor" :: "-fopenmp" :: base_flags, true)
         else
           (* No OpenMP support, just optimization flags *)
-          ([], [])
+          (base_flags, false)
       in
 
-      let final_c_flags = omp_c_flags @ base_flags in
-      let final_libs = blas_libs @ omp_libs in
+      (* Add OpenMP library to link flags if OpenMP is available *)
+      let blas_libs =
+        if has_openmp then
+          match C.ocaml_config_var c "system" with
+          | Some "macosx" ->
+              (* On macOS with Homebrew, we need to link libomp explicitly *)
+              if C.c_test c ~link_flags:[ "-lomp" ] "int main() { return 0; }"
+              then "-lomp" :: blas_libs_base
+              else blas_libs_base
+          | _ ->
+              (* On Linux, link with gomp (GNU OpenMP) *)
+              "-lgomp" :: blas_libs_base
+        else blas_libs_base
+      in
+
+      C.Flags.write_sexp "c_library_flags.sexp" blas_libs;
+      C.Flags.write_sexp "c_flags.sexp" c_flags)
 
       C.Flags.write_sexp "c_library_flags.sexp" final_libs;
       C.Flags.write_sexp "c_flags.sexp" final_c_flags)
