4fae08b6def05d011cd3f4b8c4f0cd45ac1ada1e
RidwanAdebosin <adebosinadewale@gmail.com>
2025-10-28 18:30:32 +0100

    feat(test): Add trajectory plotting and animation for DQN agent

--- fehu/test/plot_dqn_test.ml
+++ fehu/test/plot_dqn_test.ml
@@ -2,8 +2,59 @@ open Nx
 open Hugin
 
 let demo_dir = "/Users/mac/Desktop/Outreachy/raven/fehu/demos/"
+let grid_size = 5
 
-(* Helper to read positions from file *)
+let plot_trajectory_frame xs ys step filename =
+  let fig = Figure.create () in
+  let ax = Figure.add_subplot fig in
+  (* Draw grid lines *)
+  for i = 0 to grid_size do
+    let x = float_of_int i in
+    Plotting.plot ~x:(Nx.create Nx.float32 [|2|] [|x; x|])
+                  ~y:(Nx.create Nx.float32 [|2|] [|0.; float_of_int grid_size|])
+                  ~color:Artist.Color.gray
+                  ax |> ignore;
+    Plotting.plot ~x:(Nx.create Nx.float32 [|2|] [|0.; float_of_int grid_size|])
+                  ~y:(Nx.create Nx.float32 [|2|] [|x; x|])
+                  ~color:Artist.Color.gray
+                  ax |> ignore;
+  done;
+  (* Plot trajectory up to current step *)
+  let x_tensor = Nx.create Nx.float32 [| step + 1 |] (Array.sub xs 0 (step + 1)) in
+  let y_tensor = Nx.create Nx.float32 [| step + 1 |] (Array.sub ys 0 (step + 1)) in
+  Plotting.scatter ~x:x_tensor ~y:y_tensor ~c:Artist.Color.blue ~label:"Agent Path" ax |> ignore;
+  (* Mark current position *)
+  Plotting.scatter ~x:(Nx.create Nx.float32 [|1|] [|xs.(step)|])
+                   ~y:(Nx.create Nx.float32 [|1|] [|ys.(step)|])
+                   ~c:Artist.Color.orange
+                   ~label:"Current"
+                   ax |> ignore;
+  (* Mark start and goal *)
+  Plotting.scatter ~x:(Nx.create Nx.float32 [|1|] [|0.|])
+                   ~y:(Nx.create Nx.float32 [|1|] [|0.|])
+                   ~c:Artist.Color.green
+                   ~label:"Start"
+                   ax |> ignore;
+  Plotting.scatter ~x:(Nx.create Nx.float32 [|1|] [|4.|])
+                   ~y:(Nx.create Nx.float32 [|1|] [|4.|])
+                   ~c:Artist.Color.red
+                   ~label:"Goal"
+                   ax |> ignore;
+  Axes.set_xlabel "X Position" ax |> ignore;
+  Axes.set_ylabel "Y Position" ax |> ignore;
+  Axes.set_title "Agent Trajectory Animation" ax |> ignore;
+  Axes.legend true ax |> ignore;
+  Hugin.savefig filename fig
+;;
+
+let animate_episode xs ys =
+  let n = Array.length xs in
+  for step = 0 to n - 1 do
+    let filename = Printf.sprintf "%s/frame_%03d.png" demo_dir step in
+    plot_trajectory_frame xs ys step filename
+  done
+;;
+
 let read_positions path =
   let lines = In_channel.with_open_bin path In_channel.input_all
               |> String.split_on_char '\n'
@@ -17,14 +68,12 @@ let read_positions path =
   in
   (Array.of_list xs, Array.of_list ys)
 
-(* Helper to read rewards from file *)
 let read_rewards path =
   let lines = In_channel.with_open_bin path In_channel.input_all
               |> String.split_on_char '\n'
               |> List.filter (fun s -> String.trim s <> "") in
   Array.of_list (List.map float_of_string lines)
 
-(* Helper to read heatmap from file *)
 let read_heatmap path =
   let lines = In_channel.with_open_bin path In_channel.input_all
               |> String.split_on_char '\n'
@@ -44,14 +93,18 @@ let () =
   let early_xs, early_ys = read_positions (demo_dir ^ "early_positions.txt") in
   let x_tensor = Nx.create Nx.float32 [| Array.length early_xs |] early_xs in
   let y_tensor = Nx.create Nx.float32 [| Array.length early_ys |] early_ys in
-  let fig = Hugin.scatter ~label:"Agent Path" x_tensor y_tensor in
+  let fig = Figure.create () in
+  let ax = Figure.add_subplot fig in
+  Plotting.scatter ~x:x_tensor ~y:y_tensor ~c:Artist.Color.blue ~label:"Agent Path" ax |> ignore;
   Hugin.savefig (demo_dir ^ "gridworld_trajectory_early.png") fig;
 
   (* Plot late trajectory *)
   let late_xs, late_ys = read_positions (demo_dir ^ "late_positions.txt") in
   let x_tensor = Nx.create Nx.float32 [| Array.length late_xs |] late_xs in
   let y_tensor = Nx.create Nx.float32 [| Array.length late_ys |] late_ys in
-  let fig = Hugin.scatter ~label:"Agent Path" x_tensor y_tensor in
+  let fig = Figure.create () in
+  let ax = Figure.add_subplot fig in
+  Plotting.scatter ~x:x_tensor ~y:y_tensor ~c:Artist.Color.orange ~label:"Agent Path" ax |> ignore;
   Hugin.savefig (demo_dir ^ "gridworld_trajectory_late.png") fig;
 
   (* Plot episode rewards *)
@@ -66,4 +119,7 @@ let () =
   let shape = [| Array.length heatmap; Array.length heatmap.(0) |] in
   let heatmap_tensor = Nx.create Nx.float32 shape flat in
   let fig_heatmap = Hugin.imshow heatmap_tensor in
-  Hugin.savefig (demo_dir ^ "gridworld_heatmap.png") fig_heatmap
+  Hugin.savefig (demo_dir ^ "gridworld_heatmap.png") fig_heatmap;
+
+  (* Generate animation frames for early episode *)
+  animate_episode early_xs early_ys
