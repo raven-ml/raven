c093cbb3abc46fb70cfcb4d514fe51bfbd001591
Nirnay Roy <nroy1012@gmail.com>
2026-01-07 01:16:40 +0530

    added tests for mul, div and mod

--- dev/nx-oxcaml/test/test_nx_oxcaml.ml
+++ dev/nx-oxcaml/test/test_nx_oxcaml.ml
@@ -190,6 +190,137 @@ let test_in_place_add () =
   check_float64 "inplace_add[1]" ~eps:1e-9 #22.0 (get64 d 1);
   check_float64 "inplace_add[2]" ~eps:1e-9 #33.0 (get64 d 2)
 
+  let test_mul_float64 () =
+    let ctx = Nx_oxcaml.create_context () in
+    let a = Nx_oxcaml.of_float64 ctx [| #1.0; #2.0; #3.0 |] in
+    let b = Nx_oxcaml.of_float64 ctx [| #10.0; #20.0; #30.0 |] in
+    let out = Nx_oxcaml.op_buffer ctx Dtype.Float64 3 in
+    Nx_oxcaml.op_mul ~out a b;
+    let d = Nx_oxcaml.data_array out in
+    check_float64 "mul_float64[0]" ~eps:1e-9 #10.0 (get64 d 0);
+    check_float64 "mul_float64[1]" ~eps:1e-9 #40.0 (get64 d 1);
+    check_float64 "mul_float64[2]" ~eps:1e-9 #90.0 (get64 d 2)
+  
+  let test_mul_float32 () =
+    let ctx = Nx_oxcaml.create_context () in
+    let a = Nx_oxcaml.of_float32 ctx [| #1.5s; #2.5s; #3.5s |] in
+    let b = Nx_oxcaml.of_float32 ctx [| #0.5s; #0.5s; #2.0s |] in
+    let out = Nx_oxcaml.op_buffer ctx Dtype.Float32 3 in
+    Nx_oxcaml.op_mul ~out a b;
+    let d = Nx_oxcaml.data_array out in
+    check_float32 "mul_float32[0]" ~eps:1e-6 #0.75s (get32 d 0);
+    check_float32 "mul_float32[1]" ~eps:1e-6 #1.25s (get32 d 1);
+    check_float32 "mul_float32[2]" ~eps:1e-6 #7.0s (get32 d 2)
+  
+  let test_mul_int32 () =
+    let ctx = Nx_oxcaml.create_context () in
+    let a = Nx_oxcaml.of_int32 ctx [| #1l; #2l; #3l |] in
+    let b = Nx_oxcaml.of_int32 ctx [| #100l; #200l; #300l |] in
+    let out = Nx_oxcaml.op_buffer ctx Dtype.Int32 3 in
+    Nx_oxcaml.op_mul ~out a b;
+    let d = Nx_oxcaml.data_array out in
+    check_int32 "mul_int32[0]" #100l (geti32 d 0);
+    check_int32 "mul_int32[1]" #400l (geti32 d 1);
+    check_int32 "mul_int32[2]" #900l (geti32 d 2)
+  
+  let test_mul_int64 () =
+    let ctx = Nx_oxcaml.create_context () in
+    let a = Nx_oxcaml.of_int64 ctx [| #1000L; #2000L; #3000L |] in
+    let b = Nx_oxcaml.of_int64 ctx [| #1L; #2L; #3L |] in
+    let out = Nx_oxcaml.op_buffer ctx Dtype.Int64 3 in
+    Nx_oxcaml.op_mul ~out a b;
+    let d = Nx_oxcaml.data_array out in
+    check_int64 "mul_int64[0]" #1000L (geti64 d 0);
+    check_int64 "mul_int64[1]" #4000L (geti64 d 1);
+    check_int64 "mul_int64[2]" #9000L (geti64 d 2)
+
+  let test_fdiv_float64 () =
+    let ctx = Nx_oxcaml.create_context () in
+    let a = Nx_oxcaml.of_float64 ctx [| #1.0; #2.0; #2.0 |] in
+    let b = Nx_oxcaml.of_float64 ctx [| #0.0; #2.0; #3.0 |] in
+    let out = Nx_oxcaml.op_buffer ctx Dtype.Float64 3 in
+    Nx_oxcaml.op_fdiv ~out b a;
+    let d = Nx_oxcaml.data_array out in
+    check_float64 "fdiv_float64[0]" ~eps:1e-9 #0.0 (get64 d 0);
+    check_float64 "fdiv_float64[1]" ~eps:1e-9 #1.0 (get64 d 1);
+    check_float64 "fdiv_float64[2]" ~eps:1e-9 #1.5 (get64 d 2)
+  
+  let test_fdiv_float32 () =
+    let ctx = Nx_oxcaml.create_context () in
+    let a = Nx_oxcaml.of_float32 ctx [| #1.5s; #2.5s; #7.0s |] in
+    let b = Nx_oxcaml.of_float32 ctx [| #0.5s; #0.5s; #2.0s |] in
+    let out = Nx_oxcaml.op_buffer ctx Dtype.Float32 3 in
+    Nx_oxcaml.op_fdiv ~out a b;
+    let d = Nx_oxcaml.data_array out in
+    check_float32 "fdiv_float32[0]" ~eps:1e-6 #3.0s (get32 d 0);
+    check_float32 "fdiv_float32[1]" ~eps:1e-6 #5.0s (get32 d 1);
+    check_float32 "fdiv_float32[2]" ~eps:1e-6 #3.5s (get32 d 2)
+  
+  let test_fdiv_int32 () =
+    let ctx = Nx_oxcaml.create_context () in
+    let a = Nx_oxcaml.of_int32 ctx [| #1l; #2l; #3l |] in
+    let b = Nx_oxcaml.of_int32 ctx [| #100l; #1l; #2l |] in
+    let out = Nx_oxcaml.op_buffer ctx Dtype.Int32 3 in
+    Nx_oxcaml.op_fdiv ~out a b;
+    let d = Nx_oxcaml.data_array out in
+    check_int32 "fdiv_int32[0]" #0l (geti32 d 0);
+    check_int32 "fdiv_int32[1]" #2l (geti32 d 1);
+    check_int32 "fdiv_int32[2]" #1l (geti32 d 2)
+  
+  let test_fdiv_int64 () =
+    let ctx = Nx_oxcaml.create_context () in
+    let a = Nx_oxcaml.of_int64 ctx [| #1000L; #2000L; #3000L |] in
+    let b = Nx_oxcaml.of_int64 ctx [| #1L; #2L; #3L |] in
+    let out = Nx_oxcaml.op_buffer ctx Dtype.Int64 3 in
+    Nx_oxcaml.op_fdiv ~out a b;
+    let d = Nx_oxcaml.data_array out in
+    check_int64 "fdiv_int64[0]" #1000L (geti64 d 0);
+    check_int64 "fdiv_int64[1]" #1000L (geti64 d 1);
+    check_int64 "fdiv_int64[2]" #1000L (geti64 d 2)
+
+let test_idiv_float64 () =
+  let ctx = Nx_oxcaml.create_context () in
+  let a = Nx_oxcaml.of_float64 ctx [| #1.0; #2.0; #2.0 |] in
+  let b = Nx_oxcaml.of_float64 ctx [| #0.0; #2.0; #3.0 |] in
+  let out = Nx_oxcaml.op_buffer ctx Dtype.Float64 3 in
+  Nx_oxcaml.op_idiv ~out b a;
+  let d = Nx_oxcaml.data_array out in
+  check_float64 "idiv_float64[0]" ~eps:1e-9 #0.0 (get64 d 0);
+  check_float64 "idiv_float64[1]" ~eps:1e-9 #1.0 (get64 d 1);
+  check_float64 "idiv_float64[2]" ~eps:1e-9 #1.0 (get64 d 2)
+
+let test_idiv_float32 () =
+  let ctx = Nx_oxcaml.create_context () in
+  let a = Nx_oxcaml.of_float32 ctx [| #1.5s; #2.5s; #7.0s |] in
+  let b = Nx_oxcaml.of_float32 ctx [| #0.5s; #0.5s; #2.0s |] in
+  let out = Nx_oxcaml.op_buffer ctx Dtype.Float32 3 in
+  Nx_oxcaml.op_idiv ~out a b;
+  let d = Nx_oxcaml.data_array out in
+  check_float32 "idiv_float32[0]" ~eps:1e-6 #3.0s (get32 d 0);
+  check_float32 "idiv_float32[1]" ~eps:1e-6 #5.0s (get32 d 1);
+  check_float32 "idiv_float32[2]" ~eps:1e-6 #3.0s (get32 d 2)
+
+let test_idiv_int32 () =
+  let ctx = Nx_oxcaml.create_context () in
+  let a = Nx_oxcaml.of_int32 ctx [| #1l; #2l; #3l |] in
+  let b = Nx_oxcaml.of_int32 ctx [| #100l; #1l; #2l |] in
+  let out = Nx_oxcaml.op_buffer ctx Dtype.Int32 3 in
+  Nx_oxcaml.op_idiv ~out a b;
+  let d = Nx_oxcaml.data_array out in
+  check_int32 "idiv_int32[0]" #0l (geti32 d 0);
+  check_int32 "idiv_int32[1]" #2l (geti32 d 1);
+  check_int32 "idiv_int32[2]" #1l (geti32 d 2)
+
+let test_idiv_int64 () =
+  let ctx = Nx_oxcaml.create_context () in
+  let a = Nx_oxcaml.of_int64 ctx [| #1000L; #2000L; #3000L |] in
+  let b = Nx_oxcaml.of_int64 ctx [| #1L; #2L; #3L |] in
+  let out = Nx_oxcaml.op_buffer ctx Dtype.Int64 3 in
+  Nx_oxcaml.op_idiv ~out a b;
+  let d = Nx_oxcaml.data_array out in
+  check_int64 "idiv_int64[0]" #1000L (geti64 d 0);
+  check_int64 "idiv_int64[1]" #1000L (geti64 d 1);
+  check_int64 "idiv_int64[2]" #1000L (geti64 d 2)
 let () =
   print_endline "Running Nx_oxcaml backend tests...";
   test_buffer_float64 ();
@@ -208,5 +339,17 @@ let () =
   test_add_negative_values ();
   test_sub_to_zero ();
   test_in_place_add ();
+  test_mul_float64 ();
+  test_mul_float32 ();
+  test_mul_int64 ();
+  test_mul_int32 ();
+  test_fdiv_float64 ();
+  test_fdiv_float32 ();
+  test_fdiv_int64 ();
+  test_fdiv_int32 ();
+  test_idiv_float64 ();
+  test_idiv_float32 ();
+  test_idiv_int64 ();
+  test_idiv_int32 ();
   Printf.printf "\nResults: %d passed, %d failed\n" !passed !failed;
   if !failed > 0 then exit 1
