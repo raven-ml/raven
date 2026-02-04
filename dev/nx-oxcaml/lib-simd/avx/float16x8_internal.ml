9f51cdf0da5c0aa5baf8d93e71a0042a076c8d76
Nirnay Roy <nroy1012@gmail.com>
2026-02-04 20:06:21 +0530

    make avx standalone and not depend on sse

--- dev/nx-oxcaml/lib-simd/avx/float16x8_internal.ml
+++ dev/nx-oxcaml/lib-simd/avx/float16x8_internal.ml
@@ -7,49 +7,40 @@
   Released under the MIT license.
   ---------------------------------------------------------------------------*)
 
-include Ocaml_simd_sse.Float16x8
+type t = float16x8#
 
-external of_float16x16
-  :  float16x16#
-  -> t
-  @@ portable
-  = "ocaml_simd_avx_unreachable" "caml_vec256_low_to_vec128"
-[@@noalloc] [@@builtin]
+(* ───── Casts ───── *)
 
-(* These intrinsics expect [0x0..0x7], but [%float_round] generates [0x8..0xC]. *)
-module Round = struct
-  type t = int64#
+external of_int8x16 : int8x16# -> t @@ portable
+  = "caml_sse2_unreachable" "caml_vec128_cast"
+  [@@noalloc] [@@builtin]
 
-  let[@inline] nearest () = #0b000L
-  let[@inline] current () = #0b100L
-  let[@inline] down () = #0b001L
-  let[@inline] up () = #0b010L
-  let[@inline] toward_zero () = #0b011L
-end
+external of_int16x8 : int16x8# -> t @@ portable
+  = "caml_sse2_unreachable" "caml_vec128_cast"
+  [@@noalloc] [@@builtin]
 
-external of_float32x4
-  :  Round.t
-  -> float32x4#
-  -> t
-  @@ portable
-  = "ocaml_simd_avx_unreachable" "caml_f16c_cvt_float32x4_float16x8"
-[@@noalloc] [@@builtin]
+external of_int32x4 : int32x4# -> t @@ portable
+  = "caml_sse2_unreachable" "caml_vec128_cast"
+  [@@noalloc] [@@builtin]
 
-external of_float32x8
-  :  Round.t
-  -> float32x8#
-  -> t
-  @@ portable
-  = "ocaml_simd_avx_unreachable" "caml_f16c_cvt_float32x8_float16x8"
-[@@noalloc] [@@builtin]
+external of_int64x2 : int64x2# -> t @@ portable
+  = "caml_sse2_unreachable" "caml_vec128_cast"
+  [@@noalloc] [@@builtin]
 
-let[@inline] of_float32x4_nearest x = of_float32x4 (Round.nearest ()) x
-let[@inline] of_float32x4_current x = of_float32x4 (Round.current ()) x
-let[@inline] of_float32x4_down x = of_float32x4 (Round.down ()) x
-let[@inline] of_float32x4_up x = of_float32x4 (Round.up ()) x
-let[@inline] of_float32x4_toward_zero x = of_float32x4 (Round.toward_zero ()) x
-let[@inline] of_float32x8_nearest x = of_float32x8 (Round.nearest ()) x
-let[@inline] of_float32x8_current x = of_float32x8 (Round.current ()) x
-let[@inline] of_float32x8_down x = of_float32x8 (Round.down ()) x
-let[@inline] of_float32x8_up x = of_float32x8 (Round.up ()) x
-let[@inline] of_float32x8_toward_zero x = of_float32x8 (Round.toward_zero ()) x
+external of_float32x4 : float32x4# -> t @@ portable
+  = "caml_sse2_unreachable" "caml_vec128_cast"
+  [@@noalloc] [@@builtin]
+
+external of_float64x2 : float64x2# -> t @@ portable
+  = "caml_sse2_unreachable" "caml_vec128_cast"
+  [@@noalloc] [@@builtin]
+
+(* ───── Interleave ───── *)
+
+external interleave_high_16 : t -> t -> t @@ portable
+  = "caml_sse2_unreachable" "caml_simd_vec128_interleave_high_16"
+  [@@noalloc] [@@unboxed] [@@builtin]
+
+external interleave_low_16 : t -> t -> t @@ portable
+  = "caml_sse2_unreachable" "caml_simd_vec128_interleave_low_16"
+  [@@noalloc] [@@unboxed] [@@builtin]
