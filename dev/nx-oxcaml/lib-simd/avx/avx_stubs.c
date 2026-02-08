3aa9d5583204bec15250abb2042fb53e08009ce2
Thibaut Mattio <thibaut.mattio@gmail.com>
2026-02-08 12:01:03 +0000

    Update c headers

--- dev/nx-oxcaml/lib-simd/avx/avx_stubs.c
+++ dev/nx-oxcaml/lib-simd/avx/avx_stubs.c
@@ -1,11 +1,12 @@
-// ---------------------------------------------------------------------------
-//   Copyright (c) 2026 The Raven authors. All rights reserved.
-//   Distributed under the ISC license, see terms at the end of the file.
+/*---------------------------------------------------------------------------
+   Copyright (c) 2026 The Raven authors. All rights reserved.
+   SPDX-License-Identifier: ISC
 
-//   Based on ocaml_simd (https://github.com/janestreet/ocaml_simd)
-//   Copyright (c) 2025-2026 Jane Street Group, LLC
-//   Released under the MIT license.
-//   --------------------------------------------------------------------------
+   Based on ocaml_simd (https://github.com/janestreet/ocaml_simd)
+   Copyright (c) 2025-2026 Jane Street Group, LLC
+   Released under the MIT license.
+  ---------------------------------------------------------------------------*/
+
 #include <assert.h>
 
 void ocaml_simd_avx_unreachable() { assert(!"SIMD is not supported in bytecode mode."); }
