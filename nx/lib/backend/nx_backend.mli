2d243d2e2c5cdb90ee850a30989ea4b63e16965d
Thibaut Mattio <thibaut.mattio@gmail.com>
2026-02-13 16:54:49 +0000

    chore: Add a note on limited support for parametrisable devices

--- nx/lib/backend/nx_backend.mli
+++ nx/lib/backend/nx_backend.mli
@@ -5,4 +5,8 @@
 
 include Nx_core.Backend_intf.S
 
+(* TODO: [create_context : unit -> context] won't work for backends that need
+   parameters (e.g. GPU device index, memory limits). We'll likely need
+   [create_context : ?config:config -> unit -> context] or similar, with a
+   default config for each backend. *)
 val create_context : unit -> context
