acf985ff875fcc56695f51c0d986733dbc45de68
Nirnay Roy <nroy1012@gmail.com>
2026-02-17 13:28:54 +0530

    remove cast

--- dev/nx-oxcaml/test/test_nx_oxcaml.ml
+++ dev/nx-oxcaml/test/test_nx_oxcaml.ml
@@ -1279,105 +1279,6 @@ let test_pad_float64_permuted_view () =
   check_float64 "pad_float64_perm[3,1]" ~eps:1e-9 #6.0 (get64 d 10);
   check_float64 "pad_float64_perm[3,2]" ~eps:1e-9 #0.0 (get64 d 11)
 
-let test_fold_int32_1d_overlap () =
-  let ctx = Nx_oxcaml.create_context () in
-  (* Shape [N=1, C*K=2, L=2] where C=1, K=2 *)
-  let x_flat = Nx_oxcaml.of_int32 ctx [| #1l; #3l; #2l; #4l |] in
-  let x = Nx_oxcaml.op_reshape x_flat (Symbolic_shape.of_ints [| 1; 2; 2 |]) in
-  let y =
-    Nx_oxcaml.op_fold x
-      ~output_size:[| 3 |]
-      ~kernel_size:[| 2 |]
-      ~stride:[| 1 |]
-      ~dilation:[| 1 |]
-      ~padding:[| (0, 0) |]
-  in
-  let shape_y =
-    match Symbolic_shape.eval (View.shape (Nx_oxcaml.view y)) with
-    | Some s -> s
-    | None -> failwith "shape not evaluable"
-  in
-  check "fold_int32_1d_overlap: shape0" (shape_y.(0) = 1);
-  check "fold_int32_1d_overlap: shape1" (shape_y.(1) = 1);
-  check "fold_int32_1d_overlap: shape2" (shape_y.(2) = 3);
-  let d = Nx_oxcaml.data_array y in
-  check_int32 "fold_int32_1d_overlap[0]" #1l (geti32 d 0);
-  check_int32 "fold_int32_1d_overlap[1]" #5l (geti32 d 1);
-  check_int32 "fold_int32_1d_overlap[2]" #4l (geti32 d 2)
-
-let test_fold_int32_1d_padding_stride () =
-  let ctx = Nx_oxcaml.create_context () in
-  (* Shape [N=1, C*K=3, L=2] where C=1, K=3 *)
-  let x_flat = Nx_oxcaml.of_int32 ctx [| #10l; #20l; #30l; #40l; #50l; #60l |] in
-  let x = Nx_oxcaml.op_reshape x_flat (Symbolic_shape.of_ints [| 1; 3; 2 |]) in
-  let y =
-    Nx_oxcaml.op_fold x
-      ~output_size:[| 4 |]
-      ~kernel_size:[| 3 |]
-      ~stride:[| 2 |]
-      ~dilation:[| 1 |]
-      ~padding:[| (1, 1) |]
-  in
-  let d = Nx_oxcaml.data_array y in
-  check_int32 "fold_int32_1d_padding_stride[0]" #30l (geti32 d 0);
-  check_int32 "fold_int32_1d_padding_stride[1]" #70l (geti32 d 1);
-  check_int32 "fold_int32_1d_padding_stride[2]" #40l (geti32 d 2);
-  check_int32 "fold_int32_1d_padding_stride[3]" #60l (geti32 d 3)
-
-let test_unfold_int32_1d_basic () =
-  let ctx = Nx_oxcaml.create_context () in
-  let x_flat = Nx_oxcaml.of_int32 ctx [| #1l; #2l; #3l; #4l |] in
-  let x = Nx_oxcaml.op_reshape x_flat (Symbolic_shape.of_ints [| 1; 1; 4 |]) in
-  let y =
-    Nx_oxcaml.op_unfold x
-      ~kernel_size:[| 2 |]
-      ~stride:[| 1 |]
-      ~dilation:[| 1 |]
-      ~padding:[| (0, 0) |]
-  in
-  let shape_y =
-    match Symbolic_shape.eval (View.shape (Nx_oxcaml.view y)) with
-    | Some s -> s
-    | None -> failwith "shape not evaluable"
-  in
-  check "unfold_int32_1d_basic: shape0" (shape_y.(0) = 1);
-  check "unfold_int32_1d_basic: shape1" (shape_y.(1) = 2);
-  check "unfold_int32_1d_basic: shape2" (shape_y.(2) = 3);
-  let d = Nx_oxcaml.data_array y in
-  check_int32 "unfold_int32_1d_basic[0]" #1l (geti32 d 0);
-  check_int32 "unfold_int32_1d_basic[1]" #2l (geti32 d 1);
-  check_int32 "unfold_int32_1d_basic[2]" #3l (geti32 d 2);
-  check_int32 "unfold_int32_1d_basic[3]" #2l (geti32 d 3);
-  check_int32 "unfold_int32_1d_basic[4]" #3l (geti32 d 4);
-  check_int32 "unfold_int32_1d_basic[5]" #4l (geti32 d 5)
-
-let test_unfold_int32_1d_padding_stride () =
-  let ctx = Nx_oxcaml.create_context () in
-  let x_flat = Nx_oxcaml.of_int32 ctx [| #1l; #2l; #3l; #4l |] in
-  let x = Nx_oxcaml.op_reshape x_flat (Symbolic_shape.of_ints [| 1; 1; 4 |]) in
-  let y =
-    Nx_oxcaml.op_unfold x
-      ~kernel_size:[| 3 |]
-      ~stride:[| 2 |]
-      ~dilation:[| 1 |]
-      ~padding:[| (1, 1) |]
-  in
-  let shape_y =
-    match Symbolic_shape.eval (View.shape (Nx_oxcaml.view y)) with
-    | Some s -> s
-    | None -> failwith "shape not evaluable"
-  in
-  check "unfold_int32_1d_padding_stride: shape0" (shape_y.(0) = 1);
-  check "unfold_int32_1d_padding_stride: shape1" (shape_y.(1) = 3);
-  check "unfold_int32_1d_padding_stride: shape2" (shape_y.(2) = 2);
-  let d = Nx_oxcaml.data_array y in
-  check_int32 "unfold_int32_1d_padding_stride[0]" #0l (geti32 d 0);
-  check_int32 "unfold_int32_1d_padding_stride[1]" #2l (geti32 d 1);
-  check_int32 "unfold_int32_1d_padding_stride[2]" #1l (geti32 d 2);
-  check_int32 "unfold_int32_1d_padding_stride[3]" #3l (geti32 d 3);
-  check_int32 "unfold_int32_1d_padding_stride[4]" #2l (geti32 d 4);
-  check_int32 "unfold_int32_1d_padding_stride[5]" #4l (geti32 d 5)
-
 let test_shrink_int32_view () =
   let ctx = Nx_oxcaml.create_context () in
   let x_flat = Nx_oxcaml.of_int32 ctx [| #1l; #2l; #3l; #4l; #5l; #6l |] in
@@ -1432,24 +1333,6 @@ let test_cat_int32_axis1 () =
   check_int32 "cat_int32_axis1[5]" #4l (geti32 d 5);
   check_int32 "cat_int32_axis1[6]" #7l (geti32 d 6);
   check_int32 "cat_int32_axis1[7]" #8l (geti32 d 7)
-
-let test_cast_float64_to_int32 () =
-  let ctx = Nx_oxcaml.create_context () in
-  let x = Nx_oxcaml.of_float64 ctx [| #1.9; -#2.1; #0.0 |] in
-  let y = Nx_oxcaml.op_cast x Dtype.Int32 in
-  let d = Nx_oxcaml.data_array y in
-  check_int32 "cast_f64_i32[0]" #1l (geti32 d 0);
-  check_int32 "cast_f64_i32[1]" (-#2l) (geti32 d 1);
-  check_int32 "cast_f64_i32[2]" #0l (geti32 d 2)
-
-let test_cast_bool_to_float32 () =
-  let ctx = Nx_oxcaml.create_context () in
-  let x = Nx_oxcaml.of_bool ctx [| true; false; true |] in
-  let y = Nx_oxcaml.op_cast x Dtype.Float32 in
-  let d = Nx_oxcaml.data_array y in
-  check_float32 "cast_bool_f32[0]" ~eps:1e-6 #1.0s (get32 d 0);
-  check_float32 "cast_bool_f32[1]" ~eps:1e-6 #0.0s (get32 d 1);
-  check_float32 "cast_bool_f32[2]" ~eps:1e-6 #1.0s (get32 d 2)
   
 let () =
   print_endline "Running Nx_backend backend tests...";
@@ -1545,14 +1428,8 @@ let () =
   test_pad_int32_1d ();
   test_pad_float64_2d ();
   test_pad_float64_permuted_view ();
-  test_unfold_int32_1d_basic ();
-  test_unfold_int32_1d_padding_stride ();
-  test_fold_int32_1d_overlap ();
-  test_fold_int32_1d_padding_stride ();
   test_shrink_int32_view ();
   test_flip_int32_view ();
   test_cat_int32_axis1 ();
-  test_cast_float64_to_int32 ();
-  test_cast_bool_to_float32 ();
   Printf.printf "\nResults: %d passed, %d failed\n" !passed !failed;
   if !failed > 0 then exit 1
