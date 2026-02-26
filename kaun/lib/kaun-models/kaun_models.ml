a6fb85b5270861dae0127bb05500aa89fca5094a
Thibaut Mattio <thibaut.mattio@gmail.com>
2026-02-26 10:24:11 +0000

    chore(kaun): Remove kaun-models library
    Remove BERT, GPT-2, and LeNet model definitions. Model architectures
    are now defined by users or provided as examples.

--- kaun/lib/kaun-models/kaun_models.ml
+++
@@ -1,8 +0,0 @@
-(*---------------------------------------------------------------------------
-  Copyright (c) 2026 The Raven authors. All rights reserved.
-  SPDX-License-Identifier: ISC
-  ---------------------------------------------------------------------------*)
-
-module LeNet = Lenet
-module Bert = Bert
-module GPT2 = Gpt2
