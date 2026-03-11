183815a90535be84c29b2ee37fd1e414be8960dd
Thibaut Mattio <thibaut.mattio@gmail.com>
2026-03-11 08:40:50 +0000

    chore(raven): Remove raven.top package

--- packages/raven/top/raven_top.ml
+++
@@ -1,7 +0,0 @@
-(*---------------------------------------------------------------------------
-  Copyright (c) 2026 The Raven authors. All rights reserved.
-  SPDX-License-Identifier: ISC
-  ---------------------------------------------------------------------------*)
-
-(* REPL convenience package for loading the Raven stack with the default Nx
-   backend and top-level printers. *)
